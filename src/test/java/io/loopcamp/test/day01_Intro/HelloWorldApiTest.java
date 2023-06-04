f029218c1c05d300da1af1e326960729dfbc6c3f
feyruzshahmuradov <feyruz@loopcamp.io>
2023-06-04 12:50:46 -0400

    Initial commit

---
+++ src/test/java/io/loopcamp/test/day01_Intro/HelloWorldApiTest.java
@@ -0,0 +1,23 @@
+package io.loopcamp.test.day01_Intro;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+public class HelloWorldApiTest {
+    String url = "https://sandbox.api.service.nhs.uk/hello-world/hello/world";
+
+    @DisplayName("Hello World GET request")
+    @Test
+    public void helloWorldGetRequestTest(){
+        //send a get request and save response inside the Response object
+        Response response = RestAssured.get(url);
+
+        response.prettyPrint();
+
+    }
+
+}
